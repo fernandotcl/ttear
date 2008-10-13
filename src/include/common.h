18f52999f430f6efd8b87e070a2af23d07c9c57a
Fernando Lemos <fernandotcl@gmail.com>
2008-10-13 03:36:44 +0000

    More cleanup.

--- src/include/common.h
+++ src/include/common.h
@@ -1,7 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <SDL.h>
 #include <inttypes.h>
 #include <iostream>
 
@@ -23,13 +22,23 @@
 #endif
 #include <cassert>
 
+// SDL recommends this non-conventional way to include its main include file
+#include "SDL.h"
+
 #define LOGERROR (cerr << "E: ")
 #define LOGWARNING (cerr << "W: ")
 
 using namespace std;
 
+// g_junk is a variable filled with junk, used when we want some degree of
+// randomness in order to increase realism (for example, when reading from a
+// register in a state which could lead to undetermined behavior)
 extern uint8_t g_junk;
+
+// The two I/O ports, P1 and P2
 extern uint8_t g_p1, g_p2;
+
+// T1 test input (set when in VBLANK, clear otherwise)
 extern bool g_t1;
 
 #endif
